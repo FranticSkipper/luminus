c2a80c029d72817813ee0e90071fe7ff76a4038c
Oleg <oleg@gmail.com>
2025-06-03 17:19:12 +0300

    merge l-1 l-2 l-3

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
 import TiptapEditor from "../widgets/TiptapEditor/TiptapEditor";
-=======
 import Header from "../components/Header/Header";
->>>>>>> l-3
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <TiptapEditor />;
-=======
       <Header />
->>>>>>> l-3
     </>
   );
 }
--- src/widgets/TiptapEditor/TiptapEditor.tsx
+++ src/widgets/TiptapEditor/TiptapEditor.tsx
@@ -1,3 +1,28 @@
+import {
+  useEditor,
+  EditorContent,
+  FloatingMenu,
+  BubbleMenu,
+} from "@tiptap/react";
+
+import StarterKit from "@tiptap/starter-kit";
+
+// define your extension array
+const extensions = [StarterKit];
+
+const content = "<p>Hello World!</p>";
+
 export default function TiptapEditor() {
-  return "editor";
+  const editor = useEditor({
+    extensions,
+    content,
+  });
+
+  return (
+    <>
+      <EditorContent editor={editor} />
+      <FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>
+      <BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu>
+    </>
+  );
 }
