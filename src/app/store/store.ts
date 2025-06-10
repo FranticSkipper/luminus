d62d20536487ed6503288930461117964e6ff4f9
Oleg <oleg@gmail.com>
2025-06-10 16:23:18 +0300

    add redux

---
+++ src/app/store/store.ts
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispath = typeof store.dispatch;
